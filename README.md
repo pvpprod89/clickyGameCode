bb85306ef49e2e639d2c17fd1e756ed1441e24e9
Parth Patel <pvpprod89@gmail.com>
2018-10-30 20:11:03 -0400

    files/codes for repo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# clickyGameCode
-Code for ClikyGame Repo
+# Canada's Prime Ministers Memory Game
+
+- Test your memory with Canada's Prime Ministers Memory Game that includes 18th to 29th Prime Ministers name and picture.
+- Click on each picture only once to win the game. If you click twice on a same picture, then you will loose the game!
---
+++ src/components/FriendCard/FriendCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./FriendCard.css";
+
+const FriendCard = props => (
+  <div
+    className="card"
+    value={props.id}
+    onClick={() => props.handleClick(props.id)}
+  >
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+
+    <div className="img-content">
+        <strong>Name:</strong> {props.name}        
+    </div>
+
+  </div>
+);
+
+export default FriendCard;
